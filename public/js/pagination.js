152569575f19edd0d2f998126e47d23faf9a1015
RODRIGUEZ lucas <lucasrodriguez@free.fr>
2023-05-03 17:50:59 +0200

    previous and next button working for home page, adjustment needed for categories

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,71 +1,33 @@
-function makeAjaxRequest(url, method, data, successCallback, errorCallback) {
-    $.ajax({
-        url: url,
-        method: method,
-        data: data,
-        success: successCallback,
-        error: errorCallback
-    });
+function navigateToPage(page){
+    window.location.href = "/accueil?page=" + page;
 }
 
 $(document).ready(function () {
     // Call this function initially to set the correct visibility of the buttons
-    let currentPage = $('#current-page').val();
-    let totalPages = $('#total-pages').val();
+    let currentPage = parseInt($('#current-page').val());
+    let totalPages = parseInt($('#total-pages').val());
     updateButtonsVisibility(currentPage,totalPages);
 
     // Function to update the visibility of the buttons
     function updateButtonsVisibility(currentPage,totalPages) {
-        console.log(currentPage);
-        console.log(totalPages);
-        if (currentPage === 1) {
-            $('#prev-btn').hide();
-        }
-        else {
-            $('#prev-btn').show();
-        }
-
-        if (currentPage === totalPages) {
-            $('#next-btn').hide();
-        }
-        else {
-            $('#next-btn').show();
-        }
+        $("#prev-btn").prop("disabled", currentPage === 1);
+        $("#next-btn").prop("disabled", currentPage === totalPages);
     }
 
-    // Handle the click event of the Previous button
+    // Gérer le déclenchement du bouton page précédente
     $('#prev-btn').click(function () {
-        // const currentPage = $('#current-page').val();
-        // const totalPages = $('#total-pages').val();
-        console.log(currentPage);
-        console.log(totalPages);
         if (currentPage > 1) {
             currentPage--;
-            /* makeAjaxRequest("/accueil", "GET", {page: currentPage}, function (data) {
-                console.log("Success: ", data);
-            }, function (jqXHR, textStatus, errorThrown) {
-                console.log("Error: ", textStatus, errorThrown);
-            });
-             */
+            navigateToPage(currentPage);
         }
         updateButtonsVisibility(currentPage,totalPages);
     });
 
-    // Handle the click event of the Next button
+    // Gérer le déclenchement du bouton page suivante
     $('#next-btn').click(function () {
-        // const currentPage = $('#current-page').val();
-        // const totalPages = $('#total-pages').val();
-        console.log(currentPage);
-        console.log(totalPages);
         if (currentPage < totalPages) {
             currentPage++;
-            /*
-            makeAjaxRequest("/accueil", "GET", {page: currentPage}, function (data) {
-                console.log("Success: ", data);
-            }, function (jqXHR, textStatus, errorThrown) {
-                console.log("Error: ", textStatus, errorThrown);
-            });
-             */
+            navigateToPage(currentPage);
         }
         updateButtonsVisibility(currentPage,totalPages);
     });
