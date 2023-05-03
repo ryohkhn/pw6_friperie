1bf1d386c69e749b7ba967a1cd678152defbc1ba
cahagne <marius.cahagne@etu.u-paris.fr>
2023-05-03 15:53:35 +0200

    verif of the register informations

--- src/page.js
+++ src/page.js
@@ -85,6 +85,102 @@ server.get('/search',(req, res) => {
 
 });
 
+function fullLetter(text){ 
+    var letters = /^[A-Za-z]+$/;
+    if(text.value.match(letters)){
+        return true;
+    }else{
+        alert('Username must have alphabet characters only');
+        text.focus();
+        return false;
+    }
+}
+
+// Verify register informations
+
+server.post('/verify_register', (req, res) => {
+    const prenom = (req.body.inputPrenom);
+    const nom = (req.body.inputNom);
+    const email = (req.body.inputEmail);
+    const login = (req.body.inputLogin);
+    const password = (req.body.inputPassword);
+    const passwordVerif = (req.body.inputPasswordVerif);
+    const adresse = (req.body.inputAdresse);
+    const adresse2 = (req.body.inputAdresse2);
+    const ville = (req.body.inputVille);
+    const code = (req.body.inputCode);
+
+    const alphaNumericRegex = /^[a-zA-Z0-9]+$/;
+    const alphaRegex = /^[a-zA-Z]+$/;
+    const emailRegex = /\S+@\S+\.\S+/;
+    const adresseRegex = /^[a-zA-Z0-9\s,'-]+$/;
+    const codeRegex = /^\d{5}$/;
+
+
+    let errors = {};
+
+    if (!login.match(alphaNumericRegex)) {
+        errors.loginLetters= "Le pseudo doit être composé de lettres et de chiffres.";
+    }
+    if (!prenom.match(alphaRegex)) {
+        errors.prenom ="Le prénom doit être composé de lettres uniquement.";
+    }
+    if (!nom.match(alphaRegex)) {
+        errors.nom = "Le nom doit être composé de lettres uniquement.";
+    }
+    if (!email.match(emailRegex)) {
+        errors.email= "L'adresse e-mail est invalide.";
+    }
+    if (password.length < 8) {
+        errors.password= "Le mot de passe doit contenir au moins 8 caractères.";
+    }
+    if (password !== passwordVerif) {
+        errors.passwordVerif ="Les mots de passe ne correspondent pas.";
+    }
+    if (!adresse.match(adresseRegex)) {
+        errors.adresse = "L'adresse est invalide.";
+    }
+    if(adresse2){
+        if (!adresse2.match(adresseRegex)) {
+            errors.adresse2="Les informations complémentaires contiennent des caractères invalides.";
+        }
+    }
+    if (!ville.match(alphaRegex)) {
+        errors.ville= "La ville doit être composée de lettres uniquement.";
+    }
+    if (!code.match(codeRegex)) {
+        errors.code = "Le code postal doit être composé de 5 chiffres.";
+    }
+
+    
+    let request=`SELECT * FROM gerants,clients WHERE login = '${login}'`;
+    db.query(request,(err,result)=>{
+        if (err) {
+            console.log(err);
+            res.render('error.ejs',{errorcode: err});
+        }
+        else if (result.rows.length > 0) {
+            errors.loginExists = "Le pseudo entré n'est pas disponible.";
+        }
+        let emailReq = `SELECT * FROM gerants,clients WHERE email = '${email}'`;
+        db.query(emailReq, (err, result2) => {
+            if (err) {
+                console.log(err);
+                res.render('error.ejs',{errorcode: err});
+            }else if (result2.rows.length > 0) {
+                errors.emailExists = "L'adresse mail entrée n'est pas disponible.";
+            }
+            if(Object.keys(errors).length === 0){
+                //TODO : créer la session et ajouter dans la base de données
+                res.render('/');
+            }else{
+                res.render('register_page.ejs',{erreurs:errors});
+            }
+        });
+    });
+}
+);
+
 // Verify login/password
 server.post('/verify_login', (req, res) => {
     const login = (req.body.username);
